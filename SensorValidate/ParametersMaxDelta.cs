ff1d73b445e6b1629ef0828201f9c1d6c52ffea4
Vaibhav Bhapkar <vaibhavrb999@gmail.com>
2021-03-24 23:31:28 +0530

    Naming convention addition and duplication removal

---
+++ SensorValidate/ParametersMaxDelta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensorValidate
+{
+    public class ParametersMaxDelta : IParameterDelta
+    {
+        public double maxSOCDelta => 0.05;
+
+        public double maxCurrentDelta => 0.1;
+    }
+}
